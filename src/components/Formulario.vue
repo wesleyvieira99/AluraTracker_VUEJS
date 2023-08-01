99113a05b39f9b2f70be938e068836d28d5ffe99
Wesley Vieira <wesleysv11@gmail.com>
2023-08-01 15:34:27 -0300

    primeiro commit

---
+++ src/components/Formulario.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="column is-5" role="form" aria-label="Formulário para iniciar uma nova tarefa">
+        <input
+          class="input"
+          type="text"
+          placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
+        />
+      </div>
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto">
+            <option value="">Selecione o projeto</option>
+            <option
+              :value="projeto.id"
+              v-for="projeto in projetos"
+              :key="projeto.id"
+            >
+              {{ projeto.nome }}
+            </option>
+          </select>
+        </div>
+      </div>
+      <div class="column">
+        <Temporizador @aoFinalizarTarefa="salvarTarefa"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from "vue";
+import Temporizador from "./Temporizador.vue";
+import { useStore } from 'vuex'
+
+import { key } from '@/store'
+
+export default defineComponent({
+  name: "Formulario",
+  emits: ['aoSalvarTarefa'],
+  components: {
+    Temporizador,
+  },
+  setup (props, { emit }) {
+    
+    const store = useStore(key)
+
+    const descricao = ref("")
+    const idProjeto = ref("")
+
+    const projetos = computed(() => store.state.projeto.projetos)
+
+    const salvarTarefa = (tempoEmSegundos: number) : void => {    
+      emit('aoSalvarTarefa', {
+        duracaoEmSegundos: tempoEmSegundos,
+        descricao: descricao.value,
+        projeto: projetos.value.find(proj => proj.id == idProjeto.value)
+      })
+      descricao.value = ''
+    }
+
+    return {
+      descricao,
+      idProjeto,
+      projetos,
+      salvarTarefa
+    }
+  }
+});
+</script>
+<style scoped>
+.button {
+  margin-left: 8px;
+}
+.box {
+  background-color: var(--bg-primario);
+  color: var(--texto-primario);
+}
+</style>
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,56 @@
+<template>
+  <Box>
+    <div class="columns clicavel" @click="tarefaClicada">
+      <div class="column is-4">
+        {{ tarefa.descricao || 'Tarefa sem descrição' }}
+      </div>
+      <div class="column is-3">
+        {{ tarefa.projeto?.nome || 'N/D' }}
+      </div>
+      <div class="column">
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+      </div>
+    </div>
+  </Box>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from 'vue';
+import Cronometro from "./Cronometro.vue";
+import Box from "./Box.vue";
+import ITarefa from "../interfaces/ITarefa";
+
+export default defineComponent({
+  name: 'Tarefa',
+  emits: ['aoTarefaClicada'],
+  components: {
+    Cronometro,
+    Box
+  },
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true
+    }
+  },
+  setup(props, { emit }) {
+    const tarefaClicada = () : void => {
+      emit('aoTarefaClicada', props.tarefa)
+    }
+    const tempoGasto = computed(() => {
+      return new Date(props.tarefa.duracaoEmSegundos * 1000)
+        .toISOString()
+        .substr(11, 8)
+    })
+    return {
+      tarefaClicada,
+      tempoGasto
+    }
+  }
+});
+</script>
+<style scoped>
+.clicavel {
+  cursor: pointer;
+}
+</style>
